8cb32472359b851447a0c613fdd3d70aa4054232
newancroos <newmancroos@gmail.com>
2022-02-18 15:27:20 -0500

    completion of discount microservice

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,3 +1,4 @@
+using Discount.API.Extensions;
 using Discount.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//--------Migrate PostgrateDAtabase-----------------------------------
+app.MigrateDatabase<Program>();
+//----------------------------------------------------------------------
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
